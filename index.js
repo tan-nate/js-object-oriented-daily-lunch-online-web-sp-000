9327f60378e211fb51e890f044246a4a78d56e80
Nate Tan <nktan93@gmail.com>
2019-09-04 19:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class Customer {
         );
     }
     meals() {
-        return deliveries().map(delivery => delivery.meal());
+        return this.deliveries().map(delivery => delivery.meal());
     }
 }
 
