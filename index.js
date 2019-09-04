14e1df5969ec7fdf0fb7e9fb552138ef2d186bd1
Nate Tan <nktan93@gmail.com>
2019-09-04 19:47:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-function dynamicSort(property) {
-    var sortOrder = 1;
-
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-
-    return function (a,b) {
-        if(sortOrder == -1){
-            return b[property].localeCompare(a[property]);
-        }else{
-            return a[property].localeCompare(b[property]);
-        }
-    }
-}
-
 let neighborhoodId = 0;
 
 class Neighborhood {
